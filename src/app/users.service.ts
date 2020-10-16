179553f7192a72cb1fd05485b5c9e256c4e311f6
Dane Vanderbilt <danevandy@hotmail.com>
2020-10-16 15:59:28 -0500

    Minor changes

--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -8,13 +8,13 @@ export class UsersService {
 
     constructor(private counterService: CounterService) { }
 
-    onSetToInactive(id: number) {
+    setToInactive(id: number) {
         this.inactiveUsers.push(this.activeUsers[id]);
         this.activeUsers.splice(id, 1);
         this.counterService.onChangeToInactive();
     }
 
-    onSetToActive(id: number) {
+    setToActive(id: number) {
         this.activeUsers.push(this.inactiveUsers[id]);
         this.inactiveUsers.splice(id, 1);
         this.counterService.onChangeToActive();
